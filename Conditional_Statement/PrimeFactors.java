49974a95d37f562ca4e2457ec27634e677fca63d
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-10 23:15:48 +0530

    prime number
    all cases tested on this code .
    working and expected output satisfied.

--- Conditional_Statement/PrimeFactors.java
+++ Conditional_Statement/PrimeFactors.java
@@ -6,25 +6,28 @@ class PrimeFactors
 		System.out.print("Enter number to find prime factors : ");
 		int num = new Scanner(System.in).nextInt();
 		int cnt = 0;
-		for (int i=1;i<=(num/2) ;i++ )
+		int fact = 0;
+	
+		for (int i=2;i<(num/2) ;i++ )
 		{
 			if (num%i==0)
 			{
-				for (int j=2;j<=i ;j++ )
+				fact = 2;
+				while (fact<i)
 				{
-					if (i%j==0)
+					if (i%fact==0)
 					{
-						cnt++;
-						System.out.print(j+ "  ");
+						break;
 					}
+					fact++;
 				}
 			}
-		}
-		System.out.print("\n There are "+ cnt +" prime factors.");
-	
-		if (cnt==0)
+			if (fact == i)
 		{
-			System.out.print("\n There are no  prime factors of "+num);
+			System.out.print(i +"    ");
 		}
+		}
+		
+		
 	}
 }
