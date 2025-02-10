d96a514a73d4bc82ee877c465d7c8d73887a4f0f
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-10 23:25:07 +0530

    otp generation
    6 digit otp generation using random function

---
+++ loops/OtpGeneration.java
@@ -0,0 +1,19 @@
+class OtpGeneration 
+{
+	public static void main(String[] args) 
+	{
+		String otp = "";
+		
+		for (int i = 1;i<=10;)
+		{
+			otp += (int)(Math.random()*1000000);
+			if (otp.length()==6)
+			{
+				System.out.println(otp);
+				i++;
+			}
+			otp="";
+
+		}
+	}
+}
