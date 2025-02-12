09b2478cdedc2a761c5bde2de7bdef8c3c50f22b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-12 23:31:02 +0530

    Good Prime numbers
    user can find the Good prime number between the range of users choice .
    Good prime number are the prime numbers in which all the digits are prime.

---
+++ loops/GoodPrimeNumber.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class GoodPrimeNumber 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter a Start : ");
+		int start = new Scanner(System.in).nextInt();
+		if (start<=0)
+		{
+			System.out.print("Start should be from 1");
+			System.exit(0);
+		}
+		System.out.print("Enter a Range : ");
+		int range = new Scanner(System.in).nextInt();
+		int cnt = 0;
+		for (int i = start;i<=range ;i++ )
+		{
+			boolean prime = true;
+			for (int j =2;j<i ;j++ )
+			{
+				if (i%j==0)
+				{
+					prime=false;
+					break;
+				}
+			}
+			if (prime)
+				{
+					boolean good = true;
+					for (int m = i;m>0 ;m/=10 )
+					{
+						int rem = m%10;
+						if (rem<2)
+						{
+							good=false;
+							break;
+						}
+						for (int k = 2;k<rem ;k++ )
+						{
+							if (rem%k==0)
+							{
+								good = false;
+								break;
+							}
+						}
+					}
+					if (good)
+					{
+						System.out.print(i+"  ");
+						cnt++;
+					}
+				}
+		}
+		System.out.print("\n cnt of good prime number : "+cnt);
+	}
+}
