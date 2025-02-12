253fd99d268c64195c2106e747c7ac161385d37d
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-13 00:14:26 +0530

    prime numbers from range
    Take the the range from user as a start and end . find out all the prime numbers between the range given by user. return all prime numbers with exact count of numbers.

---
+++ loops/PrimeNumInRange.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class PrimeNumInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Get range from user
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = scanner.nextInt();
+		int j= 1;
+        
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        
+        for (int num = start; num <= end; num++) {
+            if (num < 2) {
+                continue;
+            }
+            boolean isPrime = true;
+            for (int i = 2; i <(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(num +"  ");
+				j++;
+            }
+        }System.out.print("\n Total prime numbers : "+j);
+    }
+}
