1d69954d57700152ae4c5f0bec6f92775569545a
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-20 22:59:04 +0530

    HollowTriangle2

--- Patterns/Triangles/HollowTriangle2.java
+++ Patterns/Triangles/HollowTriangle2.java
@@ -2,6 +2,17 @@ class HollowTriangle2
 {
 	public static void main(String[] args) 
 	{
+
+		/*
+		output
+		 
+		         *
+		      *  *
+		    *    *
+		  *      *
+		* * * *  *
+	 */
+
 		for (int i = 1;i<=5;i++ )
 		{
 			for (int j = 1;j<=(5-i) ;j++ )
