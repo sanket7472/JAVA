8b4c6d9f794aa9dd05073db427de1324f3c85724
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-21 12:19:47 +0530

    Decimal to binary
     decimal to binary conversion without in built method

---
+++ Number Series/BinaryNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class BinaryNumber 
+{
+	// decimal to binary conversion without in built method
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter number : ");
+		int num =  new Scanner(System.in).nextInt();    // 192;
+		String bin ="";
+		for (int i = num;i>0 ;i/=2 )
+		{
+			bin = i%2 + bin;
+			 
+		}
+		System.out.print(num + " in binary is : "+bin);
+	}
+}
