bb1c4087d23c7b5ecd1a3748e8c620d9522ca9ce
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-22 19:56:26 +0530

    Diamond
    A hollow diamond surrounded by * . making a square of stars and middle diamond is hollow.

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Pattern2{
+	public static void main (String[] args){
+
+		int n = 5;
+		for (int i = n;i>=0 ; i--){
+			for (int j = 0; j<=i; j++){
+				System.out.print("* ");
+			}
+			for (int j =1 ;j<=(n-i) ;j++ ) {
+				System.out.print("  ");
+			}
+			//System.out.print(" ");
+			for (int j =1 ;j<=(n-i) ;j++ ) {
+				System.out.print("  ");
+			}
+			for (int j = 0;j<=i ;j++ ) {
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		}
+		//System.out.println();
+		for (int i = 0;i<=n ; i++){
+			for (int j = 0; j<=i; j++){
+				System.out.print("* ");
+			}
+			for (int j =1 ;j<=(n-i) ;j++ ) {
+				System.out.print("  ");
+			}
+			//System.out.print(" ");
+			for (int j =1 ;j<=(n-i) ;j++ ) {
+				System.out.print("  ");
+			}
+			for (int j = 0;j<=i ;j++ ) {
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		}
+	} 
+}
