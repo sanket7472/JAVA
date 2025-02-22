db5c7c947eea64c99de239079263316f1cb13551
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-22 19:50:22 +0530

    made dry run

--- Patterns/Triangles/Triangle2.java
+++ Patterns/Triangles/Triangle2.java
@@ -3,16 +3,17 @@ class  Tringle2
 	public static void main(String[] args) 
 	{
 		System.out.println(" lower left right angle tringle");
+//                 4    4>=1
 		for (int i = 5;i>=1 ;i --)
 		{
-			// Space
+			// Space            5-4
 			for (int j = 1 ;j<=(5-i) ;j++ )
 			{
 				System.out.print("  ");
 			}
 			for (int j = 1 ;j<=i ;j++ )
 			{
-				System.out.print("* ");
+				System.out.print("* "); 
 			}
 			System.out.println();
 		}
