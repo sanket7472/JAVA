b84201964105d9acee3da778c8a0f06ab717863f
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-23 20:27:20 +0530

    code updated

--- Patterns/CharPattern3.java
+++ Patterns/CharPattern3.java
@@ -1,25 +1,20 @@
-class CharPattern3 
-{
-	public static void main(String[] args) 
-	{
+class CharPattern3 {
+	public static void main(String[] args) {
 		/*
-		output
-		1
-		0  1
-		1  0  1
-		0  1  0  1
-		*/
+		 * output
+		 * 1
+		 * 0 1
+		 * 1 0 1
+		 * 0 1 0 1
+		 */
+		System.out.println(" Charecter Pattern 3 : ");
 		int n = 4;
-		for (int i = 1;i<=n ;i++ )
-		{
-			for (int j = 1;j<=i ;j++ )
-			{
-				if ((i+j)%2==0)
-				{
-					System.out.print("1"+" ");
-				}
-				else 
-					System.out.print("0"+" ");
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				if ((i + j) % 2 == 0) {
+					System.out.print("1" + " ");
+				} else
+					System.out.print("0" + " ");
 
 			}
 			System.out.println();
