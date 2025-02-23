c171ab7cbd706b70080f734a288eef9b774f82da
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-23 20:26:24 +0530

    code updated

--- Patterns/Alphabets/E-Pattern.java
+++ Patterns/Alphabets/E-Pattern.java
@@ -5,9 +5,9 @@
         
         for (int j = 1;j<=n;j++){
             if(i==1||i==n-1||i==n/2&&j<=n/2||j==1)
-            System.out.print("* ");
+                 System.out.print("* ");
             else
-            System.out.print("  ");
+                 System.out.print("  ");
         }
         System.out.println();
     }
