281e9170b6d665f10aecad319e02940b8bd20dbf
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-23 20:14:19 +0530

    E_Pattern

---
+++ Patterns/Alphabets/E-Pattern.java
@@ -0,0 +1,15 @@
+ class E_Pattern {
+    public static void main (String[]args){
+    int n = 10;
+    for (int i = 1 ; i<=n-1;i++) {
+        
+        for (int j = 1;j<=n;j++){
+            if(i==1||i==n-1||i==n/2&&j<=n/2||j==1)
+            System.out.print("* ");
+            else
+            System.out.print("  ");
+        }
+        System.out.println();
+    }
+    }
+}
