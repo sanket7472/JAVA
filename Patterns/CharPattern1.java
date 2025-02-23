26552e2cd7070b4947738a30af4a8218730236e6
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-23 20:26:49 +0530

    code updated

--- Patterns/CharPattern1.java
+++ Patterns/CharPattern1.java
@@ -1,25 +1,23 @@
-class CharPattern1 
-{
-	public static void main(String[] args) 
-	{
-		/* output
-		a
-		b  c  
-		d  e  f
-		g  h  i  j
-		k  l  m  n  o
-		*/
+class CharPattern1 {
+	public static void main(String[] args) {
+		/*
+		 * output
+		 * a
+		 * b c
+		 * d e f
+		 * g h i j
+		 * k l m n o
+		 */
+		System.out.println(" Charecter Pattern 1 : ");
 		int n = 5;
 		char ch = 'a';
 
-	    for (int i =1 ;i<=n ;i++ )
-	    {
-			for (int j = 1;j<=i ;j++ )
-			{
-				System.out.print(ch +"  ");
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(ch + "  ");
 				ch++;
 			}
 			System.out.println();
-	    }
+		}
 	}
 }
