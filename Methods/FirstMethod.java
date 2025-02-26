17a8c08fdaca00df5d133dab11dd0c16ccf9e515
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-02-26 22:03:12 +0530

    First Method
    Started learning methods

---
+++ Methods/FirstMethod.java
@@ -0,0 +1,10 @@
+class FirstMethod  {
+	public static void main(String[] args) {
+		System.out.println(" learning Methods ");
+		firstMethod();
+	}
+	public static void firstMethod(){
+		System.out.println(" Printing from method :");
+		System.out.println(" learning binding methods and control flow of execution.");
+	}
+}
