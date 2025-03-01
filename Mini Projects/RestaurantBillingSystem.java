9b6ac3aac8d975199fc18514b285d31d9ebfac9a
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-01 22:20:38 +0530

    Working on Change order feature

--- Mini Projects/RestaurantBillingSystem.java
+++ Mini Projects/RestaurantBillingSystem.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 class RestaurantBillingSystem  {
 	
+	static ArrayList<String> basket = new ArrayList<>();
 	static Scanner sc = new Scanner(System.in);
 	static String name = "";
 	static String pass = "";
@@ -9,7 +10,8 @@ class RestaurantBillingSystem  {
 		 	System.out.println("\n       --->WELCOME<---");
         	System.out.println("       	HOTEL ADITYA ");
 			System.out.println("***********************************");
-		 	System.out.println("\n 1. CREATE ACCOUNT");
+			homepage();
+		 /*	System.out.println("\n 1. CREATE ACCOUNT");
         	System.out.println("\n 2.  LOGIN ");
         	System.out.print("\n Enter an option :");
         	int opt = sc.nextInt();
@@ -18,11 +20,11 @@ class RestaurantBillingSystem  {
         		case 1 : createAccount();break;
         		case 2 : login(); break;
         		default : System.out.println(" Enter valid option");
-        	}
+        	}*/
 		 }
 
 	}
-	public static void createAccount()
+	/*public static void createAccount()
 	{
 		System.out.println("     REGISTRATION");
 		System.out.println();
@@ -61,7 +63,7 @@ class RestaurantBillingSystem  {
 
 		}
 	
-	}
+	}*/
 
 	public static void homepage()
 	{
@@ -85,18 +87,178 @@ class RestaurantBillingSystem  {
 	}
 	public static void menu()
 	{
-		System.out.println("menu");
+		System.out.println("    Menu");
+		System.out.println("101  Paneer_Masala    450");
+		System.out.println("102  Paneer_Chili     450");
+		System.out.println("103  Butter_Paneer    450");
+		System.out.println("104  Palak_Paneer     450");
+		System.out.println("105  Paneer_Angara    450");
+		System.out.println("106  Mix_veg          450");
+		System.out.println("107  Matar_Paneer     450");
+		System.out.println("108  Veg_Kolhapuri    450");
+		System.out.println("109  Sp.Shev_Bhaji    450");
+		System.out.println("110  Aditya's_Sp.Thali    450");
+		System.out.println("111  Exit Menu");
 
+		System.out.println();
+		for (; ; ) {
+			
+		
+		System.out.println("Enter FoodId : ");
+		int opt = sc.nextInt();
+		switch (opt) {
+		 	case 101 : {
+		 		basket.add("101 : Paneer_Masala - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 102 : {
+		 		basket.add("102 : Paneer_Chili - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+			case 103 : {
+		 		basket.add("103 : Butter_Paneer - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+			case 104 : {
+		 		basket.add("104 : Palak_Paneer - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 105 : {
+		 		basket.add("105 : Paneer_Angara - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 106 : {
+		 		basket.add("106 : Mix_veg - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+
+		 	case 107 : {
+		 		basket.add("107 : Matar_Paneer - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 108 : {
+		 		basket.add("108 Veg_Kolhapuri    450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 109 : {
+		 		basket.add("109 : Sp.Shev_Bhaji - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+		 	case 110 : {
+		 		basket.add("110 : Aditya's_Sp.Thali - 450");
+		 		itemAdd();
+		 		break;
+		 	}
+			case 111 : {
+					return;
+			}
+		 	default:
+		 		System.out.println("Dish not Available ");
+		 		return ; 
+			
+		}
+
+		}
 	}
 	public static void orders()
 	{
-		System.out.println("orders");
-		
+		System.out.println();
+		System.out.println();
+		System.out.println("Your Orders");
+		if (basket.size()==0) {
+			System.out.println(" Basket is empty , Please Select some items from Menu");
+		}
+		else
+		{
+			for ( String foodItem : basket ) {
+				System.out.println(foodItem);
+			}
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println(" 1. Cancel Order");
+		System.out.println(" 2. Delete Food Items");
+		System.out.println(" 3. Proceed to Payment");
+		System.out.println();
+		System.out.print(" Chose Option  :");
+		int opt = sc.nextInt();
+		System.out.println();
+		switch (opt) {
+			case 1 : {
+				basket.clear();
+				System.out.println(" Order Canceled..!");
+				return;
+			}
+		case 2 : {
+			System.out.println(" Enter foodId : ");
+			int foodId = sc.nextInt();
+			removeItem(foodId);
+			break;
+		}
+		case 3 : {
+			payment();
+			break;
+		}
+		default:
+			System.out.println("Invalid Option ...!");
+			break;
+
+		}
+
+
 	}
 	public static void bill()
 	{
 		System.out.println("bill");
-		
+	}
+	public static void itemAdd()
+	{
+		System.out.println(" Item added to basket");
 	}
 
+	public static void removeItem(int foodId)
+	{
+		ArrayList<String> dupBasket = new ArrayList<>(basket);
+		int index = 0;
+		boolean flag = false;
+		for (String foodItem : dupBasket ) {
+			 
+			 String [] arr = foodItem.split(" ");
+			 if (arr[0].equals(foodId+"")) {
+			 	dupBasket.remove(index);
+			 	flag = true;
+			 	System.out.println("Item Deleted ");
+			 }
+			 
+			 index++;
+
+
+		}
+		if (!flag) {
+			System.out.println("Food Item not in the basket .. !");
+			System.out.println();
+		}
+		basket = dupBasket;
+
+	}
+	public static void payment()
+	{
+		double totalBill = 0;
+		for (String foodItem : basket ) {
+			 
+			String [] arr = foodItem.split(" ");
+			
+			totalBill += Double.parseDouble(arr[4]);
+			System.out.println("Your total bill Amount : "+totalBill);
+		}
+	}
 }
