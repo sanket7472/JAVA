1d064c7ef711c44fbbb7e4ce8fbf619626f0c5ad
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-02 22:06:30 +0530

    Rotate Number
    rotary number is the number which is rotate with each iteration

---
+++ Methods/RotateNum.java
@@ -0,0 +1,40 @@
+class RotateNum  {
+	/*
+		rotary number is the number which is rotate with each iteration 
+		123 ---> 312 -----> 231 ----> 123
+	*/
+	public static void main(String[] args) {
+		int num = 12345;
+		int rot = rotate(num);
+	//System.out.println(rot);
+		do{
+			System.out.println(rot);
+			  rot = rotate(rot);
+			
+
+		}while (rot != rotate(num));	
+				// 12345 != 51234
+	}
+	public static int rotate(int num){
+		int last = num%10;
+		num/=10;
+		int ct = count(num);
+		return last*power(ct)+num;
+	}
+	public static int count(int num)
+	{
+		int ct = 0;
+		while (num!=0){
+			ct++;
+			num/=10;
+		}
+		return ct;
+	}
+	public static int power(int ct){
+		int pow = 1;
+		for (int i = 1;i<=ct ;i++ ) {
+			pow *= 10;
+		}
+		return pow;
+	}
+}
