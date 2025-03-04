280210aa180c512ec5fb511af3947a4fc4bc92fa
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-04 10:11:11 +0530

    code updated

--- Methods/Fibonacci.java
+++ Methods/Fibonacci.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 class Fibonacci {
+	// fibonacii number series generated from 0 to till the user entered count 
+	// if user entered 10 ----> program will generate first 10 fibonacii number will printed.
 	public static void main(String[] args) {
 		System.out.print("Enter how many numbers you want to print from fibonacci series :");
 		int num = new Scanner(System.in).nextInt();
 		fibonacii(num);
 	}
-	public static void fibonacii(int num )
+	public static void fibonacii(int num)
 	{
 		int fnum = 0;
 		System.out.print(fnum +"  ");
 		int snum = 1;
 		System.out.print(snum +"  ");
-		for (int i = 2;i<num ;i++ ) {
+		for (int i = 2;i<num ;i++ )
+		{
 			int nxtnum = fnum+snum;
 			System.out.print(nxtnum +"  ");
 			fnum = snum;
