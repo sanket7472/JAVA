27229d47f28f57705345b371581969d431bfc57e
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-04 10:13:09 +0530

    Fibonacci From Range

---
+++ Methods/FibonacciFromRange.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class FibonacciFromRange {
+	// fibonacii number series will generate from user entered start to till the user entered end  
+	// if user entered 1 and  10 ----> program will generate and print number
+	// between 1 to 10 from fibonacii series.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Start :");
+		int start = sc.nextInt();
+		System.out.print("Enter end :");
+		int end = sc.nextInt();
+		fibonacii(start,end);
+	}
+	public static void fibonacii(int start ,int end )
+	{
+		int fnum = 0;
+		if (start==0) 
+		{
+			System.out.print(fnum +"  ");		
+		}
+		int snum = 1;
+		if (start<=1) 
+		{
+			System.out.print(snum +"  ");
+		}
+		int nxtnum = 0;
+		do 
+		{  
+			if (nxtnum>=start) {
+				System.out.print(nxtnum +"  ");
+			}
+			 nxtnum = fnum + snum;
+			fnum = snum;
+			snum = nxtnum;
+			
+		}while(nxtnum<=end);
+		/*
+		for (int nxtnum = 0;nxtnum<=end ; )
+		{
+			nxtnum = fnum+snum;
+			if (nxtnum>=start) {
+				System.out.print(nxtnum +"  ");
+			}
+			fnum = snum;
+			snum = nxtnum;
+		}*/
+	}
+}
