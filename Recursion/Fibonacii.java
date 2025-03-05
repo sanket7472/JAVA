2e6aeb7377bffbf3c8912bdf16fcc5a912444606
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-05 22:17:56 +0530

    Fibonacii series with recursion

---
+++ Recursion/Fibonacii.java
@@ -0,0 +1,18 @@
+class Fibonacii{
+
+	public static void main(String[] args) {
+		fibo(20 , 0 ,1);
+	}
+	public static void fibo(int num , int n1 , int n2)
+	{
+		if (num==0) {
+			return;
+		}
+		System.out.print(n1+ "  ");
+		int n3 = n2+n1;
+		fibo(--num , n2 , n3);
+	
+	
+	}
+
+}
