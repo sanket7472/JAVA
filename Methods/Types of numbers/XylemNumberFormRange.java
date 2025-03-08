c5aaba667e755485c6d24e5d51c06f4ec4f2e31a
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-08 13:33:52 +0530

    Xylem Number Form Range

---
+++ Methods/Types of numbers/XylemNumberFormRange.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class XylemNumberFormRange
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Start :");
+		int start = sc.nextInt();
+		System.out.print("Enter End : ");
+		int end = sc.nextInt();
+		for (int i = start;i<=end ;i++ ) 
+		{
+			if (isXylem(i))
+			{
+					System.out.print(i + "  ");
+			}
+		}
+	}
+	public static boolean isXylem(int num)
+	{
+		int sumOfExternal = num%10;
+		int sumOfMiddle = 0;
+		num/=10;
+		while (num>9)
+		{
+			sumOfMiddle += (num%10);
+			num/=10;
+		}
+		sumOfExternal += num;
+		return sumOfMiddle == sumOfExternal;
+	}
+}
