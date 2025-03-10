ff5fd435f480e03520dcdc25840d1704cff9d73b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-10 21:51:37 +0530

    Palindrome Number

---
+++ Recursion/PalindromeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class PalindromeNumber 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter number :");
+		int num = new Scanner(System.in).nextInt();
+		if (isPalindrome(num))
+		{
+			System.out.print(num + " is a palindrome number");
+			return;
+		}
+		System.out.print(" is not palindrome number ");
+	}
+	public static boolean isPalindrome(int num)
+	{
+		int rev = reverse(num , 0);
+		return num == rev ?true : false;
+	}
+	public static int reverse(int num , int rev )
+	{
+		return num == 0 ? rev : reverse (num/10 , (rev*10)+(num%10));
+	}
+}
