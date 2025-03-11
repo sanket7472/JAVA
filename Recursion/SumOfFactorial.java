0a4ac956a48d1e708a1ceb1cc64c280d81328229
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-11 21:35:58 +0530

    SumOfFactorial

---
+++ Recursion/SumOfFactorial.java
@@ -0,0 +1,25 @@
+class SumOfFactorial 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(sum(145 , 0));
+	}
+	public static int sum(int num , int sum )
+	{
+		// normal method
+		/*
+		if (num == 0)
+		{
+			return sum ;
+		}
+		sum += fact(num%10);
+		return sum(num/10 , sum);
+		*/
+		// optimize method
+		return num == 0 ? sum : sum(num/10 , (sum += fact(num%10)));
+	}
+	public static int fact(int num)
+	{
+		return num==1 ? 1 : num * fact(num-1);
+	}
+}
