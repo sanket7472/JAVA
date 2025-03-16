6b1d165370022c553adc3489bec92c801413e7fa
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-16 22:07:44 +0530

    Update PalindromeNumber.java

--- Recursion/PalindromeNumber.java
+++ Recursion/PalindromeNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class PalindromeNumber 
-{
+{// this program helps to find palindrome number
 	public static void main(String[] args) 
 	{
 		System.out.print("Enter number :");
