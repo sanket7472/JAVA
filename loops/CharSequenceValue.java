d47552cf4568d873896981e93793d7d3e8b300a4
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-20 21:18:53 +0530

    Update CharSequenceValue.java

--- loops/CharSequenceValue.java
+++ loops/CharSequenceValue.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 class CharSequenceValue 
 {
+	//charecter sequence
+	//A - 1
+	// AA - 27
 	public static void main(String[] args) 
 	{
 		System.out.print("Enter character (max-2) : ");
