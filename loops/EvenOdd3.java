db06fc16993b0833370dc8cd0014d984b014caa4
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-20 21:22:40 +0530

    Update EvenOdd3.java

--- loops/EvenOdd3.java
+++ loops/EvenOdd3.java
@@ -2,6 +2,7 @@ class EvenOdd3
 {
 	public static void main(String[] args) 
 	{
+		//Even number printing is easy because increment by 2 is gives only even numbers
 		// 1 to 100 Even numbers
 		System.out.println("Even Numbers in 1 to 100 :  ");
 		for (int i=2;i<=100 ;i+=2 )
