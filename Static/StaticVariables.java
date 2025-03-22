584039470c7bfd59eccfd28717dc4f80e8df167b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-22 22:52:59 +0530

    default values of all data types

---
+++ Static/StaticVariables.java
@@ -0,0 +1,25 @@
+class StaticVariables 
+{
+	// this code is written for getting the default value of all data types 
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static char c;
+	static float f;
+	static double d;
+	static boolean bl;
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Default value of byte is : "+ b);
+		System.out.println("Default value of short is : "+ s);
+		System.out.println("Default value of int is : "+ i);
+		System.out.println("Default value of long is : "+ l);
+		System.out.println("Default value of char is : "+ c);
+		System.out.println("Default value of float is : "+ f);
+		System.out.println("Default value of double is : "+ d);
+		System.out.println("Default value of boolean is : "+ bl);
+
+	}
+}
