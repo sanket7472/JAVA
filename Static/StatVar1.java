4e189e3e0d38f45bb5f7d34631ed08cc3c2c86da
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-22 22:53:53 +0530

    Using values of static variable in methos

---
+++ Static/StatVar1.java
@@ -0,0 +1,13 @@
+class StatVar1 
+{
+	static int a;
+	public static void main(String[] args) 
+	{
+		System.out.println(a);
+		test();
+	}
+	public static void test()
+	{
+		System.out.print(a);
+	}
+}
