8cdfefba1710b0a3491b0c134c009c74673e2588
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-22 22:54:56 +0530

    learning how a single variable can change every time

---
+++ Static/StatVar2.java
@@ -0,0 +1,31 @@
+class StatVar2 
+{
+	static double balance;
+	public static void main(String[] args) 
+	{
+		credit(1000);
+		debit(500);
+	}
+	public static void credit(int amt)
+	{
+		if (amt>0)
+		{
+			balance += amt;
+			System.out.println("Amount Credited ");
+			System.out.print("Total account balance is "+balance);
+		}
+		else
+			System.out.println("Invalid amount ");
+	}
+		public static void debit(int amt)
+	{
+		if (amt>0 && balance>=amt)
+		{
+			balance -= amt;
+			System.out.println("Amount Debited ");
+			System.out.print("Total account balance is "+balance);
+		}
+		else
+			System.out.println("Invalid amount ");
+	}
+}
