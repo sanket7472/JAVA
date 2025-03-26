3279907c191fbd5924b2d189e9eae40556167fd0
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-26 21:47:43 +0530

    Butterfly pattern

---
+++ Patterns/Butterfly.java
@@ -0,0 +1,57 @@
+class Butterfly 
+{
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		for (int i = 1;i<=n ;i++ )
+		{
+			for (int j = 1;j<=n ; j++ )
+			{
+				if (j<=i)
+				{
+					System.out.print("* ");
+				}
+				else
+					System.out.print("  ");
+				
+			}
+			for (int j = n;j>=1 ; j-- )
+			{
+				if (j<=i)
+				{
+					System.out.print("* ");
+				}
+				else
+					System.out.print("  ");
+				
+			}
+			System.out.println();
+		}
+		System.out.print("        * * ");
+		System.out.println();
+		for (int i = n;i>=1 ;i-- )
+		{
+			for (int j = 1;j<=n ; j++ )
+			{
+				if (j<=i)
+				{
+					System.out.print("* ");
+				}
+				else
+					System.out.print("  ");
+				
+			}
+			for (int j = n;j>=1 ; j-- )
+			{
+				if (j<=i)
+				{
+					System.out.print("* ");
+				}
+				else
+					System.out.print("  ");
+				
+			}
+			System.out.println();
+		}
+	}
+}
