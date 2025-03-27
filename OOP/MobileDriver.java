25691417a316191adc3ad8bf08156ce6e3c2de98
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-27 23:18:45 +0530

    mobile objects

---
+++ OOP/MobileDriver.java
@@ -0,0 +1,50 @@
+class MobileDriver 
+{
+	public static void main(String[] args) 
+	{
+		Mobile m1 = new Mobile();
+		m1.brand = "Samsung";
+		m1.model = "S23 Ultra " ;
+		m1.ram = 8;
+		m1.rom = 128;
+		m1.price = 69999;
+		m1.numOfCameras = 3;
+		System.out.println(m1.brand);
+		System.out.println(m1.model);
+		System.out.println(m1.ram);
+		System.out.println(m1.rom);
+		System.out.println(m1.price);
+		System.out.println(m1.numOfCameras);
+		System.out.println("----------------------------------------------");
+
+		Mobile m2 = new Mobile();
+		m2.brand = "Apple";
+		m2.model = "Iphone 16 pro " ;
+		m2.ram = 12;
+		m2.rom = 512;
+		m2.price = 150999;
+		m2.numOfCameras = 3;
+		System.out.println(m2.brand);
+		System.out.println(m2.model);
+		System.out.println(m2.ram);
+		System.out.println(m2.rom);
+		System.out.println(m2.price);
+		System.out.println(m2.numOfCameras);
+		System.out.println("----------------------------------------------");
+
+		Mobile m3 = new Mobile();
+		m3.brand = "Vivo";
+		m3.model = "V15 Pro " ;
+		m3.ram = 8;
+		m3.rom = 128;
+		m3.price = 25999;
+		m3.numOfCameras = 2;
+		System.out.println(m3.brand);
+		System.out.println(m3.model);
+		System.out.println(m3.ram);
+		System.out.println(m3.rom);
+		System.out.println(m3.price);
+		System.out.println(m3.numOfCameras);
+		System.out.println("----------------------------------------------");
+	}
+}
