3fee4b0d0c8d454132f16bd203efd0bf7ae59497
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-28 21:43:27 +0530

    implemented printing methods

--- OOP/Car.java
+++ OOP/Car.java
@@ -8,6 +8,44 @@ class Car
 	int cc;
 	int seats;
 
+	public void carCompany()
+	{
+		System.out.println("Company : "+company);
+	}
+	public void carFuel()
+	{
+		System.out.println("Fuel : "+fuel);
+	}
+	public void carPrice()
+	{
+		System.out.println("Price : "+price);
+	}
+	public void carModel()
+	{
+		System.out.println("Model Name : "+model);
+	}
+	public void carMileage()
+	{
+		System.out.println("Mileage : "+mileage);
+	}
+	public void carCC()
+	{
+		System.out.println("Engine CC : "+cc);
+	}
+	public void carSeats()
+	{
+		System.out.println("Seats in Car : "+seats);
+	}
+	public void carDetails()
+	{
+		carCompany();
+		carFuel();
+		carPrice();
+		carModel();
+		carMileage();
+		carCC();
+		carSeats();
+	}
 	
 
 }
