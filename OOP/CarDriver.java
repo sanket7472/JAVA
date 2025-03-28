3d187ee1f5df46872ac0d10fcf0700be47182b81
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-28 21:43:55 +0530

    created a car object

---
+++ OOP/CarDriver.java
@@ -0,0 +1,17 @@
+class CarDriver 
+{
+	public static void main(String[] args) 
+	{
+		Car c1 = new Car();
+
+		c1.company = "Mahendra";
+		c1.fuel = "CNG";
+		c1.price = 500000;
+		c1.model= "Thar";
+		c1.mileage = 25;
+		c1.cc = 2184;
+		c1.seats = 5;
+
+		c1.carDetails();
+	}
+}
