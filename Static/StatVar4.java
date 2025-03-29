8b3aab6fae4eba1ec98b62eb900ea4795f64debc
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-29 22:51:48 +0530

    Static variables

--- Static/StatVar4.java
+++ Static/StatVar4.java
@@ -1,13 +1,15 @@
 class StatVar4 
 {
 	static String s;
+	// defining a variable
 	public static void main(String[] args) 
 	{
+		// calling the staic variable
 		System.out.println(s);
 		demo();
 		String s = "Sam";
-		System.out.println(s);
-		System.out.println(StatVar4.s);
+		System.out.println(s); // here the local variable is called
+		System.out.println(StatVar4.s); // here calling the static variable using class reference
 		
 	}
 	public static void demo()
