895345f4a4d7f2e7b99f94097ac0cdfef5cf9145
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-31 09:30:19 +0530

    learning about static initializers

--- Static/StatInitializers.java
+++ Static/StatInitializers.java
@@ -1,7 +1,8 @@
 class StatInitializers 
 {
 	// static initializers are the static variables declare and initialize at the same time
-
+	// static declare and initialization statement is a variable declare and and intialize in 
+	// the static block or in the class block 
 	static double pi = 22.0/7.0 ;
 	public static void main(String[] args) 
 	{
