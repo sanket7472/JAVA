66823ca420d95c18320f254c76bf1b028d316dd7
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-02 21:48:26 +0530

    Driver or object class

---
+++ OOP/ShoeDriver.java
@@ -0,0 +1,13 @@
+class ShoeDriver 
+{
+	public static void main(String[] args) 
+	{
+		Shoe s1 = new Shoe();
+		
+		s1.size = 9;
+		s1.type = "Sports";
+		s1.color = "Grey";
+
+		s1.shoeDetails();
+	}
+}
