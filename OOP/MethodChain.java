4443c30fb20d6c63fd54f503681ab045d9c6c401
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-02 21:49:02 +0530

    learning method chaining

---
+++ OOP/MethodChain.java
@@ -0,0 +1,31 @@
+class MethodChain 
+{
+	String origin;
+	String type;
+	int price;
+
+	public MethodChain price()
+	{
+		System.out.println(price);
+		return this ;
+	}
+	public MethodChain origin()
+	{
+		System.out.println(origin);
+		return this ;
+	}
+	public MethodChain type()
+	{
+		System.out.println(type);
+		return this ;
+	}
+
+	public MethodChain showDetails()
+	{
+		price();
+		origin();
+		type();
+		return this ;
+	}
+
+}
