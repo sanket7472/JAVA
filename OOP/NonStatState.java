c34a64a5407c673625e96045c64aba5a385fdef0
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-02 21:46:50 +0530

    Non Static variables

---
+++ OOP/NonStatState.java
@@ -0,0 +1,18 @@
+class NonStatState
+{
+	
+		String brand = "Nike";
+		String type;
+		int size;
+		double price;
+	
+	public void showDetails()
+	{
+		//String brand = "Adidas";
+		System.out.println(brand);
+		System.out.println(type);
+		System.out.println(size);
+		System.out.println(price);
+	}
+
+}
