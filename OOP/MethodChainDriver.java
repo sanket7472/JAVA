e8afe0039b91d22be050865438f15a7c99d85bb8
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-02 21:52:04 +0530

    object creation class

---
+++ OOP/MethodChainDriver.java
@@ -0,0 +1,13 @@
+class MethodChainDriver 
+{
+	public static void main(String[] args) 
+	{
+		MethodChain m1 = new MethodChain();
+
+		m1.origin = "Ratnagiri";
+		m1.type = "Hapus";
+		m1.price = 2000;
+
+		m1.price().origin().type();
+	}
+}
