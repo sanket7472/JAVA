7b1c3d04cd855774b74f126d3c1c801bbb254e30
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-04 22:24:11 +0530

    Chaining blueprint

---
+++ OOP/Methods.java
@@ -0,0 +1,24 @@
+class Methods
+{
+	// learning about method chaining
+	public Methods m1()
+	{
+		System.out.println("From first method");
+		return this;
+	}
+	public Methods m2()
+	{
+		System.out.println("From Second method");
+		return this;
+	}
+	public Methods m3()
+	{
+		System.out.println("From third method");
+		return this;
+	}
+	public Methods m4()
+	{
+		System.out.println("From fourth method");
+		return this;
+	}
+}
