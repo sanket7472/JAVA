3f2f905542cc39b16982b435292c4783335afda8
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-06 21:44:11 +0530

    driver class

---
+++ OOP/src/Form1Driver.java
@@ -0,0 +1,11 @@
+class  Form1Driver
+{
+	public static void main(String[] args) 
+	{
+		Form1 f1 = new Form1();
+		f1.displayInfo();
+
+		Form1 f2 = new Form1("Sam", 654321846l, 'm', "12/2/20",894656666l, "o+", "sam@gamil.com");
+		f2.displayInfo();
+	}
+}
