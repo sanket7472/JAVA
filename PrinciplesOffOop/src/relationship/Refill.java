b1591a545a44cdb57a6713f062fa1fadfe036b76
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-04-11 22:23:39 +0530

    Showing relationship between Refill and Pen

---
+++ PrinciplesOffOop/src/relationship/Refill.java
@@ -0,0 +1,24 @@
+package relationship;
+
+public class Refill {
+	
+	String color;
+	String type;
+	double size;
+	
+	public Refill(String color , String type, double size) {
+		
+		this.color = color;
+		this.type = type;
+		this.size = size;
+		
+	}
+	
+
+
+	public void getRefill() {
+		System.out.println(color);
+		System.out.println(type);
+		System.out.println(size);
+	}
+}
