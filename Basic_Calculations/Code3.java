6ee370b0eed99ee6ec0a847ed094c1707a5ce38c
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-12 23:47:06 +0530

    Predict the output

---
+++ Basic_Calculations/Code3.java
@@ -0,0 +1,12 @@
+// Q - Predict the output of following
+class Code3
+{
+	public static void main(String[] args) 
+	{
+		int a= 2;
+		int b = 3;
+		int c = a++ * ++b + --a;
+		System.out.println("value of c is : "+ c);
+	}
+}
+// output - 10;
