4e298a350dc27fbc9790fc79af9b10f1aec022e5
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-12 23:47:15 +0530

    Predict the output

---
+++ Basic_Calculations/Code4.java
@@ -0,0 +1,11 @@
+// Q - Predict the output of following
+class Code4
+{
+	public static void main(String[] args) 
+	{
+		int a=4;
+		a += a *= 2;
+		System.out.println("value of a is : "+ a);
+	}
+}
+// output - 12;
