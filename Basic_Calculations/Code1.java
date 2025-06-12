3b98d54d77799240058eccca78b3b3595da75cdf
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-12 23:46:48 +0530

    Predict the output

---
+++ Basic_Calculations/Code1.java
@@ -0,0 +1,11 @@
+// Q - Predict the output of following
+class Code1 
+{
+	public static void main(String[] args) 
+	{
+		int x= 7;
+		x = x++ + x++ + ++x;
+		System.out.println("value of x is : "+ x);
+	}
+}
+// output - 25;
