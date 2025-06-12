086a9d21d9e0bd752369fbe9a79f9fcbce4daf7b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-12 23:46:57 +0530

    Predict the output

---
+++ Basic_Calculations/Code2.java
@@ -0,0 +1,11 @@
+// Q - Predict the output of following
+class Code2 
+{
+	public static void main(String[] args) 
+	{
+		int a= 10;
+		int b = --a - a--;
+		System.out.println("value of b is : "+ b);
+	}
+}
+// output - 0;
