c2eeff266d30512b4d2935e2fc0e560d349eec7b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-13 22:00:45 +0530

    Predict the output

---
+++ Basic_Calculations/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+    static int x = 10;
+ 	   public static void main(String[] args) {
+ 	       Test t1 = new Test();
+        Test t2 = new Test();
+        t1.x = 20;
+        System.out.println(t2.x);
+    }
+}
