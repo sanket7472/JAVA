a6b778939a5813c8151c94bae0f35350ea238ed1
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-13 22:00:15 +0530

    Predict the output

---
+++ Basic_Calculations/Code6.java
@@ -0,0 +1,13 @@
+// Q - Predict the output of following
+class Code6
+{
+	public static void main(String[] args) 
+	{
+		int x = 5;
+		x += (x++)+(++x); 
+		
+		System.out.println("value of x is : "+ x);
+		
+	}
+}
+// output - 17;
