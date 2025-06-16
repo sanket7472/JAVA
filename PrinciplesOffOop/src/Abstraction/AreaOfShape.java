91d609adff14768353ee6544a51d0612aae40917
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-16 22:02:40 +0530

    Create AreaOfShape.java

---
+++ PrinciplesOffOop/src/Abstraction/AreaOfShape.java
@@ -0,0 +1,9 @@
+package Abstraction;
+
+public abstract class AreaOfShape {
+
+	public abstract void areaOfCircle(double r);
+	
+	public abstract void perimeterOfCircle(double r); // circumference 
+	
+}
