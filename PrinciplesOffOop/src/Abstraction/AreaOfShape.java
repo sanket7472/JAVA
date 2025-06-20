d04a605302e09353db026b16dac27bdfc438a640
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-20 22:22:10 +0530

    Update AreaOfShape.java

--- PrinciplesOffOop/src/Abstraction/AreaOfShape.java
+++ PrinciplesOffOop/src/Abstraction/AreaOfShape.java
@@ -4,6 +4,12 @@ public abstract class AreaOfShape {
 
 	public abstract void areaOfCircle(double r);
 	
-	public abstract void perimeterOfCircle(double r); // circumference 
+	public abstract void areaOfRectangle(double l, double b); 
+	
+	public abstract void areaOfSquare(double l);
+	
+	public abstract void areaOfParallelogram(double l, double h);
+	
+	public abstract void areaOfRhombus(double b, double h); // diagonal multiplication 
 	
 }
