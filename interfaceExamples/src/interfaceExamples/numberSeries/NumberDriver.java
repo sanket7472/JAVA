fa6f3d5642c2bfc2395f76c0e175dafa1b338f32
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-22 22:02:36 +0530

    Create NumberDriver.java

---
+++ interfaceExamples/src/interfaceExamples/numberSeries/NumberDriver.java
@@ -0,0 +1,11 @@
+package interfaceExamples.numberSeries;
+
+public class NumberDriver {
+
+	public static void main(String[] args) {
+		
+		Numbers n1 = new NumbersImp();
+		
+		n1.returnNumber();
+	}
+}
