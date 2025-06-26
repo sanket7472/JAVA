9e756a53526966f48380719880a45a3157a848fe
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-26 21:29:43 +0530

    Create AreaOfCircleDriver.java

---
+++ ObjectClass/src/OOP/AreaOfCircleDriver.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class AreaOfCircleDriver {
+
+	public static void main(String[] args) {
+		
+		AreaOfCircle a1 = new AreaOfCircle(10);
+		
+		a1.areaofCircle();
+		AreaOfCircle a2 = new AreaOfCircle(15);
+		
+		a2.areaofCircle();
+		
+	}
+}
