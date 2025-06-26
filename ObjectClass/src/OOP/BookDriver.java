c976a7883f30403ba2c2b5579f50dbc47e3179ba
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-26 21:30:33 +0530

    Create BookDriver.java

---
+++ ObjectClass/src/OOP/BookDriver.java
@@ -0,0 +1,40 @@
+class BookDriver 
+{
+	public static void main(String[] args) 
+	{
+		// creating object using no argument constructor 
+		Book b1 = new Book();
+		System.out.println(b1);
+		System.out.println(b1.brand);
+		System.out.println(b1.price);
+		System.out.println(b1.pages);
+		System.out.println(b1.type);
+		System.out.println("---------------------------------------");
+
+
+		// initializing non static variables
+		b1.brand = "Classmate";
+		b1.price = 50;
+		b1.pages = 100;
+		b1.type = "long note";
+		System.out.println(b1);
+		System.out.println(b1.brand);
+		System.out.println(b1.price);
+		System.out.println(b1.pages);
+		System.out.println(b1.type);
+		System.out.println("---------------------------------------");
+		
+
+		// second object
+		Book b2 = new Book();
+		b2.brand = "Youva";
+		b2.price = 50;
+		b2.pages = 100;
+		b2.type = "long note";
+		System.out.println(b2);
+		System.out.println(b2.brand);
+		System.out.println(b2.price);
+		System.out.println(b2.pages);
+		System.out.println(b2.type);
+	}
+}
