92c3748b7de58d16df41fac22b44046e43009cdc
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-26 21:30:00 +0530

    Create Book.java

---
+++ ObjectClass/src/OOP/Book.java
@@ -0,0 +1,16 @@
+class Book 
+{
+	String brand;
+	int price;
+	int pages;
+	String type;
+	
+	// no argumented constructor added 
+	// this is programmers duty to add no argumented constructor
+	Book()
+	{}
+	Book(String brand, int price, int pages, String type)
+	{
+		System.out.println("Book Created .....");
+	}
+}
