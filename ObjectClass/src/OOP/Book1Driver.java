5930b4402a5d2c67b5635ede6a56d517f8a2e449
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-26 21:30:21 +0530

    Create Book1Driver.java

---
+++ ObjectClass/src/OOP/Book1Driver.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Book1Driver {
+
+	public static void main(String[] args) {
+		
+		Book1 b1 = new Book1("The Hidden Hindu ", "Mr. Akshat Gupta", "Mythology", "Hindi", 500);
+		
+		b1.showDetails();
+	}
+}
