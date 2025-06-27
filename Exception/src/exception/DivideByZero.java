69033f0cf6c8ea457a8afe99fdd69ad0bac16556
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-27 22:41:04 +0530

    Create DivideByZero.java

---
+++ Exception/src/exception/DivideByZero.java
@@ -0,0 +1,40 @@
+package exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivideByZero {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numerator=0;
+		int denominator = 0;
+		
+		while (true) {
+			try {
+				System.out.println("Enter Numerator : ");
+				 numerator = sc.nextInt();
+			
+				
+			
+				System.out.println("Enter denominator : ");
+				denominator = sc.nextInt();
+				sc.close();
+				
+				int ans = numerator/denominator;
+				System.out.println(ans);
+			}
+			catch (InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("Only int data is allowed ");
+			} 
+			catch (ArithmeticException e) {
+				System.out.println("Cant divide by zero");
+			}
+			catch (Throwable e) {
+				System.exit(0);
+			}
+		}
+		
+	}
+}
