4c03c21b6e029c10105491f8af742ff2442e54b3
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-28 21:29:53 +0530

    Create CarDriver.java

---
+++ ObjectClass/src/OOP/CarDriver.java
@@ -0,0 +1,26 @@
+class CarDriver 
+{
+	public static void main(String[] args) 
+	{
+		// creating object using no argument constructor
+		Car c1 = new Car();
+
+		c1.company = "Mahendra";
+		c1.fuel = "CNG";
+		c1.price = 500000;
+		c1.model= "Thar";
+		c1.mileage = 25;
+		c1.cc = 2184;
+		c1.seats = 5;
+
+	
+		// calling the method for performing actions
+		carCompany();
+		carFuel();
+		carPrice();
+		carModel();
+		carMileage();
+		carCC();
+		carSeats();
+	}
+}
