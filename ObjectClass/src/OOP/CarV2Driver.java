97e7459627b7fc5173a99d9556699dab82404d50
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-28 21:30:34 +0530

    Create CarV2Driver.java

---
+++ ObjectClass/src/OOP/CarV2Driver.java
@@ -0,0 +1,15 @@
+class CarV2Driver 
+{
+	public static void main(String[] args) 
+	{
+		CarV2 c1 = new CarV2("XUV " , "Black",150,8,25);
+
+		c1.showDetails();
+
+		System.out.println("-------------------------------");
+
+		CarV2 c2 = new CarV2("THAR " , "Black",230.5,4,40);
+
+		c2.showDetails();
+	}
+}
