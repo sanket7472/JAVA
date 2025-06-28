b9c29281fec68da9161e84e20bef6644dd79c7c0
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-28 21:29:46 +0530

    Create Car.java

---
+++ ObjectClass/src/OOP/Car.java
@@ -0,0 +1,56 @@
+class Car 
+{
+	String company;
+	String fuel;
+	int price;
+	String model;
+	int mileage;
+	int cc;
+	int seats;
+
+	// no argumented constructor added 
+	// this is programmers duty to add no argumented constructor
+	
+	Car()
+	{}
+	public void carCompany()
+	{
+		System.out.println("Company : "+company);
+	}
+	public void carFuel()
+	{
+		System.out.println("Fuel : "+fuel);
+	}
+	public void carPrice()
+	{
+		System.out.println("Price : "+price);
+	}
+	public void carModel()
+	{
+		System.out.println("Model Name : "+model);
+	}
+	public void carMileage()
+	{
+		System.out.println("Mileage : "+mileage);
+	}
+	public void carCC()
+	{
+		System.out.println("Engine CC : "+cc);
+	}
+	public void carSeats()
+	{
+		System.out.println("Seats in Car : "+seats);
+	}
+	public void carDetails()
+	{
+		carCompany();
+		carFuel();
+		carPrice();
+		carModel();
+		carMileage();
+		carCC();
+		carSeats();
+	}
+	
+
+}
