2a511890a341dca60e3461045890827f7816fbef
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-06-29 22:06:08 +0530

    Create MethodChain.java

---
+++ ObjectClass/src/OOP/MethodChain.java
@@ -0,0 +1,35 @@
+class MethodChain 
+{
+	String origin;
+	String type;
+	int price;
+	// implemented method chaining 
+	// in this method is compulsory to return the referance of object
+	// here this keyword is use to return the reference
+	MethodChain()
+	{}
+	public MethodChain price()
+	{
+		System.out.println(price);
+		return this ;
+	}
+	public MethodChain origin()
+	{
+		System.out.println(origin);
+		return this ;
+	}
+	public MethodChain type()
+	{
+		System.out.println(type);
+		return this ;
+	}
+
+	public MethodChain showDetails()
+	{
+		price();
+		origin();
+		type();
+		return this ;
+	}
+
+}
