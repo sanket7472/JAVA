4b20db1cfd1c1a468841155b7046f69fdfc4ebcd
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-01 22:00:31 +0530

    Create Mobile.java

---
+++ ObjectClass/src/OOP/Mobile.java
@@ -0,0 +1,23 @@
+class Mobile 
+{
+	String brand;
+	String model;
+	byte ram;
+	short rom;
+	int price;
+	int numOfCameras;
+	
+	// no argument constructor
+	Mobile()
+	{}
+	public void showMobile()
+	{
+		System.out.println(brand);
+		System.out.println(model);
+		System.out.println(ram);
+		System.out.println(rom);
+		System.out.println(price);
+		System.out.println(numOfCameras);
+		System.out.println("----------------------------------------------");
+	}
+}
