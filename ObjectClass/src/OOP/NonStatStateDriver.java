348d1b8b1a277fbdd8ec4b179d2948e17933e7bd
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-01 22:01:03 +0530

    Create NonStatStateDriver.java

---
+++ ObjectClass/src/OOP/NonStatStateDriver.java
@@ -0,0 +1,12 @@
+class NonStatStateDriver 
+{
+	public static void main(String[] args) 
+	{
+		NonStatState ns1 = new NonStatState();
+		ns1.type = "Formal";
+		ns1.size = 9;
+		ns1.price = 1000.0;
+
+		ns1.showDetails();
+	}
+}
