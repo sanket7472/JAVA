fd06f605f9046dfbcabf4beeee5e3e9e2f4931db
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-02 21:59:15 +0530

    Create RectangleDriver.java

---
+++ ObjectClass/src/OOP/RectangleDriver.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class RectangleDriver {
+
+	public static void main(String[] args) {
+		
+		Rectangle r1 = new Rectangle(15.0, 14.7);
+		
+		r1.area();
+		r1.perimeter();
+	}
+}
