bb7b47fa6479d0e2628818f82d5db22b98e49f41
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-03 21:06:13 +0530

    Create CarV2.java

---
+++ OOP/src/CarV2.java
@@ -0,0 +1,32 @@
+class CarV2
+{
+	 
+	 String model;
+	 String color;
+	 double speed;
+	 int seats;
+	 int fuelCap;
+
+	 public CarV2(){}
+
+	 public CarV2(String model,String color, double speed,int seats, int fuelCap) {
+			this.model = model;
+			this.color = color;
+			this.speed = speed;
+			this.seats = seats;
+			this.fuelCap = fuelCap;
+	}
+
+	public void showDetails(){
+	
+		System.out.println("Model : "+model);
+		System.out.println("Color : "+color);
+		System.out.println("Speed : "+speed);
+		System.out.println("Seats : "+seats);
+		System.out.println("Fuel Capacity : "+fuelCap + " ltrs");
+
+
+	}
+
+
+}
