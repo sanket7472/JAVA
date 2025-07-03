2dd0362868ad68f4060bb202b5953158dadac269
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-03 21:05:00 +0530

    Create Student1Driver.java

---
+++ ObjectClass/src/OOP/Student1Driver.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Student1Driver {
+
+	public static void main(String[] args) {
+		
+		Student1 s1 = new Student1();
+		
+//		s1.averageMarks();
+//		
+//		System.out.println("-----------------------------");
+//		Student1 s2 = new Student1();
+//		
+//		s2.averageMarks();
+	}
+}
