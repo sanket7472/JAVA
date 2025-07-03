f5140b01e94421874621f11121e80d49627a4e0a
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-03 21:05:06 +0530

    Create StudentDriver.java

---
+++ ObjectClass/src/OOP/StudentDriver.java
@@ -0,0 +1,13 @@
+class StudentDriver 
+{
+	public static void main(String[] args) 
+	{
+		Student s1 = new Student();
+		//here the no argumented constructor will call and bind 
+		s1.displayStudent();
+		System.out.println("----------------------------------");
+		Student s2 = new Student("Sanket",21,"SVIT College","BE",123);
+		// here the argumented contructor will call and bind
+		s2.displayStudent();
+	}
+}
