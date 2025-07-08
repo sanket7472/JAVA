e3f855e54dccb98d5b398ad9bcd3ffd60819519f
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-08 21:21:40 +0530

    Create Final5.java

---
+++ AccessModifiers/src/finalKeyword/Final5.java
@@ -0,0 +1,21 @@
+package finalKeyword;
+
+public class Final5 extends Final4{
+
+	// overriding of final methods is not allowed in sub class
+	
+//	public void test() {
+//		
+//	}
+//	// shadowing of final methods is not allowed in sub class
+//	public void demo() {
+//		
+//	}
+	
+	public static void main(String[] args) {
+		
+		new Final4().test();
+		
+		Final4.demo();
+	}
+}
