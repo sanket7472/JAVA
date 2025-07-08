f540fff0e74b8446793ac8d162b48ed7ed877743
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-08 21:21:36 +0530

    Create Final4.java

---
+++ AccessModifiers/src/finalKeyword/Final4.java
@@ -0,0 +1,21 @@
+package finalKeyword;
+
+public class Final4 {
+
+	// working of finai methods 
+	
+	// 1) non static final method 
+	
+	public final void test() {
+		System.out.println("From non static final method");
+	}
+	// 2) static final method
+	public static final void demo() {
+		System.out.println("From static final method ");
+	}
+	
+	//  3) abstract methods 
+	
+	//public abstract final void med();//CTE
+	//bcz of a member either final or abstract both modifiers not allow
+}
