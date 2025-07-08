2afc139a1ed80e7faf200ae186ce01d398f06bb9
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-08 21:21:47 +0530

    Create Final6.java

---
+++ AccessModifiers/src/finalKeyword/Final6.java
@@ -0,0 +1,12 @@
+package finalKeyword;
+
+public final class Final6 {
+
+	public static void main(String[] args) {
+		System.out.println("From main of final class");
+	}
+	
+	public void demo() {
+		System.out.println("From demo of final class");
+	}
+}
