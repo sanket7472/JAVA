8620a8f75d2060a0b5b56a148a45aadfe5d686bf
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-08 21:21:51 +0530

    Create Final7.java

---
+++ AccessModifiers/src/finalKeyword/Final7.java
@@ -0,0 +1,7 @@
+package finalKeyword;
+
+public class Final7 {//extends Final6{
+
+	// if we tried to inherit the final class we gwt an error
+	//The type Final7 cannot subclass the final class Final6
+}
