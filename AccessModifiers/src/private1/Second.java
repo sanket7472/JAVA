0ec33c348a32da7230254e286dde3c68ef643ed2
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-10 21:49:29 +0530

    Create Second.java

---
+++ AccessModifiers/src/private1/Second.java
@@ -0,0 +1,14 @@
+package private1;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// accessing the private members of class First 
+		
+		// First f1 = new First();
+		
+		// here we get a CTE as constructor of First is not visible 
+		// bcz this members are private and can not access outside the class	
+	
+	}
+}
