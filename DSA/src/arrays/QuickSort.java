b30918360b3dd47e1aa434f95e9cba56a8ec049d
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-19 22:15:09 +0530

    Create QuickSort.java

---
+++ DSA/src/arrays/QuickSort.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		int [] ar  = {50,20,60,10,40,70,30};
+		
+	}
+	public static int[] quickSort(int[]ar) {
+		
+		for (int i = 0,j=ar.length-1; i < ar.length; ) {
+			
+			
+		}
+		return ar;
+	}
+}
