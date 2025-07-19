5f3a8eb897cb2f25493a6b555b7565e1854b3a94
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-19 22:15:21 +0530

    Create RemoveDuplicates.java

---
+++ DSA/src/arrays/RemoveDuplicates.java
@@ -0,0 +1,57 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {10,20,20,10,40,40,50,40,50,40,50};
+		System.out.println(Arrays.toString(removeDuplicate(ar)));
+	}
+	public static int[] countArray(int[]ar) {
+		 
+		int [] count = new int[ar.length];
+		for (int i = 0; i < ar.length; i++) {
+
+			if (count[i] != -1) {
+				for (int j = i+1; j < ar.length; j++) {
+					
+					if (ar[i]==ar[j]) {
+						count[j] = -1;
+					}
+				}
+			}
+			
+		}
+		return count;
+	}
+	
+	public static int[] removeDuplicate(int[]ar) {
+		
+		int [] count = countArray(ar);
+		
+		int negCnt = 0;
+		for (int i = 0; i < ar.length; i++) {
+			
+			if (count[i]==-1) {
+				negCnt++;
+			}
+		}
+		if (negCnt>0) {
+			
+			int [] ans = new int [ar.length-negCnt];
+			
+			for (int i = 0,j=0; i < ar.length; i++) {
+				
+				if (count[i] != -1) {
+					
+					ans[j] = ar[i];
+					j++;
+				}
+			}
+			return ans;
+		}
+		return ar;
+	}
+}
