d62273253dbc76066e898bd3c432001f3ae87dec
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-28 22:40:15 +0530

    Create TwoDiArray.java

---
+++ DSA/src/arrays/twoDArray/TwoDiArray.java
@@ -0,0 +1,18 @@
+package arrays.twoDArray;
+
+public class TwoDiArray {
+
+	public static void main(String[] args) {
+		
+		int [][] ar = {
+				{0000},{40,50,60},{70,80,90,100}
+				
+		};
+		
+		for (int i = 0; i < ar[0].length; i++) {
+			
+			System.out.print(ar[0][i]+" ");
+		}
+	}
+	
+}
