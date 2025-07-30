b0d6ba63053a1f63fe7ac14e8f2a7c90f5dc5c1b
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-30 21:41:55 +0530

    Create MaxMinValues.java

---
+++ ObjectClass/src/wrapperClass/MaxMinValues.java
@@ -0,0 +1,34 @@
+package wrapperClass;
+
+public class MaxMinValues {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Byte MAX value : "+ Byte.MAX_VALUE);
+		System.out.println("Byte MIN value : "+ Byte.MIN_VALUE);
+		
+		System.out.println("Short MAX value : "+ Short.MAX_VALUE);
+		System.out.println("Short MIN value : "+ Short.MIN_VALUE);
+		
+		System.out.println("Integer MAX value : "+ Integer.MAX_VALUE);
+		System.out.println("Integer MIN value : "+ Integer.MIN_VALUE);
+		
+		System.out.println("Long MAX value : "+ Long.MAX_VALUE);
+		System.out.println("Long MIN value : "+ Long.MIN_VALUE);
+		
+		System.out.println("Character MAX value : "+ Character.MAX_VALUE);
+		System.out.println("Character MIN value : "+ Character.MIN_VALUE);
+		
+		System.out.println("Float MAX value : "+ Float.MAX_VALUE);
+		System.out.println("Float MAX-EXP value : "+ Float.MAX_EXPONENT);
+		System.out.println("Float MIN value : "+ Float.MIN_VALUE);
+		System.out.println("Float MIN-EXP value : "+ Float.MIN_EXPONENT);
+		
+		System.out.println("Double MAX value : "+ Double.MAX_VALUE);
+		System.out.println("Double MIN value : "+ Double.MIN_VALUE);
+
+		System.out.println("Double MAX-Exponent value : "+ Double.MAX_EXPONENT);
+		System.out.println("Double MIN-Exponent value : "+ Double.MIN_EXPONENT);
+	
+	}
+}
