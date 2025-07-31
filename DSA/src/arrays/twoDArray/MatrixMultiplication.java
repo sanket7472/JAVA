0ccfdd9b9be742af649ac2c8154673227f7328e8
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-07-31 20:49:15 +0530

    Update MatrixMultiplication.java

--- DSA/src/arrays/twoDArray/MatrixMultiplication.java
+++ DSA/src/arrays/twoDArray/MatrixMultiplication.java
@@ -18,23 +18,51 @@ public class MatrixMultiplication {
 				
 		};
 		
-		int [][] mul = multiply(a, b);
-		for (int i = 0; i < mul.length; i++) {
-			System.out.println(Arrays.toString(mul[i]));
+		try {
+			int [][] mul = multiply(a, b);
+			for (int i = 0; i < mul.length; i++) {
+				System.out.println(Arrays.toString(mul[i]));
+			}
+		} catch (NullPointerException e) {
+			System.out.println("Arrays not symmetric ");
+			System.out.println("Only symmetric arrays allowed");
 		}
 	}
 	public static int[][] multiply(int[][]a,int[][]b){
 		
-		int [][] c= new int [a.length][a[0].length];
+		if (isSymmetric(a, b)&&a[0].length==b.length) {
+			int [][] c= new int [a.length][b[0].length];
+			
+			  for (int i = 0; i < a.length; i++) { 
+		            for (int j = 0; j < b[0].length; j++) {
+		                   for (int k = 0; k < c.length; k++) {
+		                	   c[i][j] += a[i][k] * b[k][j];
+						}                                                                                 
+		            }
+		        }
+			return c;
+		}
+		return null;
+	}
+	
+	public static boolean isSymmetric(int[][]a,int[][]b) {
+		boolean check1 = true , check2 = true;
 		
-		  for (int i = 0; i < a.length; i++) { 
-	            for (int j = 0; j < a.length; j++) {
-	                   for (int k = 0; k < c.length; k++) {
-	                	   c[i][j] += a[i][k] * b[k][j];
-					}                                                                                 
-	            }
-	        }
-		return c;
+		for (int i = 0; i < a.length; i++) {
+			if (a[0].length!=a[i].length) {
+				check1 = false;
+				return false;
+				
+			}
+		}
+		for (int i = 0; i < b.length; i++) {
+			if (b[0].length!=b[i].length) {
+				check2 = false;
+				return false;
+				
+			}
+		}
+		return check1 && check2;
 	}
 	
 	
