76d094b883bde5b6ae1e68c15f26663080bb4ab5
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-02 21:25:41 +0530

    Create NeuroSuper.java

---
+++ PrinciplesOffOop/src/inheritance/NeuroSuper.java
@@ -0,0 +1,32 @@
+package inheritance;
+
+public class NeuroSuper extends Doctor {
+
+	String organ;
+	String spl;
+	
+	public NeuroSuper() {}
+	
+	public NeuroSuper(String name, double fees,String degree, int exp, String hospital, String organ, String spl ) {
+		
+		super(name, fees, degree, exp, hospital );
+		
+//		this.name = name;
+//		this.fees = fees;
+//		this.degree = degree;
+//		this.exp = exp;
+//		this.hospital = hospital;
+		
+		this.organ = organ;
+		this.spl = spl;
+	}
+	
+	public void displayNeuroSuper() {
+		
+		displayDoctor();
+		
+		System.out.println(organ);
+		System.out.println(spl);
+
+	}
+}
