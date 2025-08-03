a3873217f83ea5265e1ebe735883266f0a04d1f2
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-03 21:32:55 +0530

    Create PrintAllChar.java

---
+++ DSA/src/strings/PrintAllChar.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class PrintAllChar {
+
+	public static void main(String[] args) {
+		
+		String s = "I Love India";
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(s.charAt(i)+" ");
+		}
+	}
+}
