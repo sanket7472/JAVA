d522fe2787fb12b8d6fb3b4a51fd0fd95a1efe97
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-08 22:38:49 +0530

    Create Shapes.java

---
+++ PrinciplesOffOop/src/interfaceExamples/Shapes.java
@@ -0,0 +1,14 @@
+package interfaceExamples;
+
+public interface Shapes {
+
+	double pi = 22.0/7.0;
+	
+	void areaOfCicle(int rad);
+	
+	void perimeterOfCIrle(int rad);
+	
+	void areaOfSquare(int side);
+	
+	void perimeterOfSquare(int side);
+}
