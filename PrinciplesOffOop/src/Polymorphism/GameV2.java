65441650c56f57a4471738fbd0e2865f23265481
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-11 21:43:28 +0530

    Create GameV2.java

---
+++ PrinciplesOffOop/src/Polymorphism/GameV2.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class GameV2 extends Game {
+
+	public void turnLeft() {
+		System.out.println("Turned Left..!");
+		
+	}
+	
+	public void moveBackword(int steps){
+		
+		System.out.println("went backword "+steps +" steps");
+	}
+}
