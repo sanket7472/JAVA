b7e7eca715fafe4666b2afaca71c8d19ef08a294
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-18 21:33:09 +0530

    Create SortCharacters.java

---
+++ DSA/src/strings/SortCharacters.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class SortCharacters {
+
+	public static void main(String[] args) {
+		String s = "sanket";
+		System.out.println(sort(s));
+	}
+	public static String sort(String s) {
+		byte [] ans = s.getBytes();
+		for (int i = 0; i < ans.length; i++) {
+			for (int j = 0; j < ans.length-i-1; j++) {
+				if (ans[j]>ans[j+1]) {
+					byte temp = ans[j];
+					ans[j]=ans[j+1];
+					ans[j+1]=temp;
+				}                                                       
+			}
+		}
+		return new String(ans);
+	}
+}
