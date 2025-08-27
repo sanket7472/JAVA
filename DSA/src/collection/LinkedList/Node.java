cdff58dfe1f1938c7465168ec0614d72b0005f50
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-27 14:24:35 +0530

    Update Node.java

--- DSA/src/collection/LinkedList/Node.java
+++ DSA/src/collection/LinkedList/Node.java
@@ -2,5 +2,16 @@ package collection.LinkedList;
 
 public class Node {
 
+	Object val;
+	Node prevRef;
+	Node nxtref;
+	
+	private Node() {
+	}
+	public Node(Object val) {
+		super();
+		this.val = val;
+	}
+	
 	
 }
