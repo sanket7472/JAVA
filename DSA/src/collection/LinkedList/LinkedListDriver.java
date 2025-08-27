bf2d6b0dd85746a05971340809b65cfbab575e54
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-27 14:24:30 +0530

    Create LinkedListDriver.java

---
+++ DSA/src/collection/LinkedList/LinkedListDriver.java
@@ -0,0 +1,18 @@
+package collection.LinkedList;
+
+public class LinkedListDriver {
+
+	public static void main(String[] args) {
+		
+		LinkedList l1 = new LinkedList();
+		l1.addNode(10);
+		l1.addNode(20);
+		l1.addNode(30);
+		l1.addNode(40);
+		l1.showList();
+//		System.out.println("__________________");
+		//l1.removeNode(30);
+	//	l1.showList();
+		
+	}
+}
