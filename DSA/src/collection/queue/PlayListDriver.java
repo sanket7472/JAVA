ca0bf169d20f92d8a3a30e5c10f24bebf6505112
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-27 14:24:51 +0530

    Update PlayListDriver.java

--- DSA/src/collection/queue/PlayListDriver.java
+++ DSA/src/collection/queue/PlayListDriver.java
@@ -7,9 +7,9 @@ public class PlayListDriver {
 		PlayList p1 = new PlayList("morning mix", 10);
 		
 		p1.addSong("Hanuman Chalisa", "Arijit Singh", "5:30", "Kumar", "Roy");
-		p1.addSong("Hariapath", "Arijit Singh", "4:19", "Amitabh Bhattacharya", "Roy");
+		p1.addSong("Haripath", "Arijit Singh", "4:19", "Amitabh Bhattacharya", "Roy");
 		p1.addSong("Janm Zala varkari", "Udit Narayan", "3:07", "Hariharan", "Roy");
-		p1.addSong("Ram siya ram ", "Udit Narayan", "3:07", "Hariharan", "Roy");
+		p1.addSong("Ram siya ram", "Udit Narayan", "3:07", "Hariharan", "Roy");
 		p1.addSong("Shree Swami Samarth", "Udit Narayan", "3:15", "Hariharan", "Roy");
 		p1.addSong("Shiv tandav stotram ", "Udit Narayan", "3:15", "Hariharan", "Roy");
 		p1.addSong("Shree Krishn govind hare murari ", "Udit Narayan", "3:15", "Hariharan", "Roy");
@@ -18,9 +18,10 @@ public class PlayListDriver {
 		//p1.showPlaylist();
 		//p1.removeSong("Haripath", "Roy");
 		
-		p1.showPlaylist();
-		System.out.println("--------------------------");
-		p1.shuffle();
+//		p1.showPlaylist();
+//		System.out.println("--------------------------");
+//		p1.shuffle();
+		p1.playNext("haripath", "roy");
 		p1.showPlaylist();
 	}
 }
