45aa1f3c92e220aeb24848a3f9390753ead0428d
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-29 13:10:34 +0530

    Update LinkedListDriver.java

--- DSA/src/collection/LinkedList/LinkedListDriver.java
+++ DSA/src/collection/LinkedList/LinkedListDriver.java
@@ -5,14 +5,20 @@ public class LinkedListDriver {
 	public static void main(String[] args) {
 		
 		LinkedList l1 = new LinkedList();
+		LinkedList l2 = new LinkedList();
 		l1.addNode(10);
-		l1.addNode(20);
-		l1.addNode(30);
-		l1.addNode(40);
-		l1.showList();
-//		System.out.println("__________________");
-		//l1.removeNode(30);
-	//	l1.showList();
+		l1.addNode(50);
+		l1.addNode(30.55);
+		l1.addNode(2);
+		l1.addNode(2);
+		System.out.println(l1.contains(30.55));
+		l1.sort(l1);
+		System.out.println(l1);
+		
+		
+		
+
+
 		
 	}
 }
