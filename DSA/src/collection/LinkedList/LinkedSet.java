a98959652b6e13d4a240fee68751fb74885b66d4
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-29 13:11:15 +0530

    Create LinkedSet.java

---
+++ DSA/src/collection/LinkedList/LinkedSet.java
@@ -0,0 +1,42 @@
+package collection.LinkedList;
+
+public class LinkedSet extends LinkedList {
+
+	public LinkedSet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// addval
+	//addall
+	//addfist
+	//add last
+	public boolean addNode(Object val) {
+		if (!contains(val)) {
+			return super.addNode(val);
+		}
+		return false;
+	}
+	public boolean addFirst(Object val) {
+		if (!contains(val)) {
+			return super.addFirst(val);
+		}
+		return false;
+	}
+	public boolean addLast(Object val) {
+		if (!contains(val)) {
+			return super.addLast(val);
+		}
+		return false;
+	}
+	public boolean addAll(LinkedList l) {
+		Node head = l.iterate();
+		while (head!=null) {
+			if (!contains(head.val)) {
+				return super.addNode(head.val);
+			}
+			head = head.next();
+		}
+		return true;
+	}
+}
