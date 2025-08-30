6f6b263d6f14394c89b0acb009cf25b9bd699865
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-30 21:20:36 +0530

    Create SortedLinkedListDriver.java

---
+++ DSA/src/collection/LinkedList/SortedLinkedListDriver.java
@@ -0,0 +1,17 @@
+package collection.LinkedList;
+
+public class SortedLinkedListDriver {
+
+	public static void main(String[] args) {
+		
+		SortedLinkedList l1 = new SortedLinkedList();
+		l1.addNode(50);
+		l1.addNode(40);
+		l1.addNode(30);
+		l1.addNode(20);
+		l1.addNode(10);
+		
+		System.out.println(l1);
+		
+	}
+}
