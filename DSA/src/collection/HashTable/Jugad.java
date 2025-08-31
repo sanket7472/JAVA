6bd410641cd96e66b58d628db3b5a6e32c33be1a
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-08-31 22:14:06 +0530

    Create Jugad.java

---
+++ DSA/src/collection/HashTable/Jugad.java
@@ -0,0 +1,17 @@
+package collection.HashTable;
+
+public class Jugad {
+
+	public static void main(String[] args) {
+		/*
+		 * Java claims that the hashcode method generates
+		 * the unique code for every object 
+		 * but their is a bug in hashCode method of String class 
+		 * hashcode method gives same hashcode for Strings "FB" and "Ea"
+		 * */
+		String s1 = "FB";
+		String s2 = "Ea";
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+	}
+}
